f735cbc915d156d99b47611612f5e9821785b9c9
1fuji2watori <xvghtvv@gmail.com>
2024-07-28 17:44:15 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,6 +1,10 @@
-let num = 21;
+let num = 30;
 
-if(num % 3 === 0) {
+if(num % 3 === 0 && num % 5 ===0){
+    console.log('3と5の倍数です');
+}
+
+else if(num % 3 === 0) {
     console.log('3の倍数です');
 }
 
@@ -8,9 +12,6 @@ else if(num % 5 === 0) {
     console.log('5の倍数です');
 }
 
-else if(num % 3 === 0 && num % 5 ===0){
-    console.log('3と5の倍数です');
-}
 
 else{
     console.log(num);
